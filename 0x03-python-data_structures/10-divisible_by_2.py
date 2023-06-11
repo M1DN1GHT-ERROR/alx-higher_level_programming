09e5c92d3046dca9b94ee2f182b1c49de5bdd77b
imanouu <ilagsoun021@gmail.com>
2023-06-11 14:29:41 -0700

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,4 @@
+
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []  # Initialize an empty list to store the results
-    
-    for num in my_list:
-        result.append(num % 2 == 0)
-        # Check if the number is divisible by 2 and append the result
-    
-    return result
+    return [not i & 1 for i in my_list]
