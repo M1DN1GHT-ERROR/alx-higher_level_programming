788c7bca80ae8424343162c54742ff0055ce75fe
midnight <ilagsoun021@gmail.com>
2023-06-26 05:15:41 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for item in my_list:
+            if count < x:
+                print(item, end="")
+                count += 1
+            else:
+                break
+        print()
+        return count
+    except:
+        print("An error occurred.")
+        return count
