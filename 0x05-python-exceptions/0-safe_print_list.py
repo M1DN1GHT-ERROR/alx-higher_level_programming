8ccf5071807de6e12e1d0d6a74d2102ff1d49825
midnight <ilagsoun021@gmail.com>
2023-06-26 05:19:02 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
-    i = 0
-    count = 0
-
-    while i < x:
-        try:
-            print(my_list[i], end='')
-            count = count + 1
-
-        except IndexError:
-            return count
-
-        i = i + 1
-    print('\n')
-    return cou
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
