e9485608c437a8f9f1138f94d7ff76e16f2b56e2
midnight <ilagsoun021@gmail.com>
2023-07-11 09:34:30 -0700

    emyy

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -34,14 +34,14 @@ try:
         try:
             status = int(pieces[-2])
 
-            if str(status) in status_codes.keys():
+            if str(status) in status_codes:
                 status_codes[str(status)] += 1
-        except:
+        except ValueError:
             pass
 
         try:
             file_size += int(pieces[-1])
-        except:
+        except ValueError:
             pass
 
         lc += 1
@@ -50,3 +50,4 @@ try:
 except KeyboardInterrupt:
     print_info()
     raise
+
